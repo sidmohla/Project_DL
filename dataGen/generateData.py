4a371b2e03f57d115534cce9ea5fd865f5d203fd
broodingmonk98 <es16btech11014@iith.ac.in>
2018-03-25 21:16:04 +0530

    Initial commit

---
+++ dataGen/generateData.py
@@ -0,0 +1,41 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import networkx as nx
+
+G = nx.complete_graph(10)
+e = np.array(G.edges)
+X,y = [],[]
+count = 0;
+for i in range(10000):
+    idx= np.random.choice(len(e),14);
+    edg = e[idx];
+    G = nx.empty_graph(10);
+    G.add_edges_from(edg)
+    mat = np.zeros((10,10),dtype=int);
+    for i in edg:
+        mat[tuple(i)]=True;
+        mat[tuple(i[::-1])]=True;
+    X.append(mat.reshape(1,-1)[0]);
+    #print(mat)
+
+    label = nx.is_connected(G)
+    y.append(int(label))
+    count += label
+    #print(i,label)
+    #nx.draw(G);
+    #plt.show();
+
+y = np.asarray(y)
+
+import pickle
+pickle_out = open('data_connected_graph.pickle','wb')
+pickle.dump((X,y),pickle_out);
+pickle_out.close();
+pickle_in  = open('data_connected_graph.pickle','rb')
+X,y = pickle.load(pickle_in);
+print(type(X),type(X[0][0]))
+print("X shape",len(X),X[0].shape)
+print("y shape",len(y))
+
+print("DONE")
+print("# Connected :",count)
---
+++ readme.md
@@ -0,0 +1,4 @@
+Here we try to look at which learning problems are easy for deep learning
+
+The dataGen folder contains the python scripts to generate our data
+The Train folder, contains a simple neural network which we try to fit over our data
