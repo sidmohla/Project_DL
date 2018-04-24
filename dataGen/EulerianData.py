a54dce3275ee421721d6729a48a22f5f7ecf374d
sidmohla <sidmohla@gmail.com>
2018-04-24 17:54:50 +0530

    Update EulerianData.py

--- dataGen/EulerianData.py
+++ dataGen/EulerianData.py
@@ -4,21 +4,13 @@ import matplotlib.pyplot as plt
 import networkx as nx
 
 def is_semieulerian(G):
-    # Check if all non-zero degree vertices are connected
     if nx.is_connected(G) == False:
         return 0
     else:
-        #Count vertices with odd degree
         odd = 0
         for i in range(G.number_of_nodes()):
-            #print(len(G.adj[i]))
             if len(G.adj[i]) % 2 !=0:
                 odd +=1
-
-        '''If odd count is 2, then semi-eulerian.
-        If odd count is 0, then eulerian
-        If count is more than 2, then graph is not Eulerian
-        Note that odd count can never be 1 for undirected graph'''
         if odd == 0:
             return 1
         elif odd == 2:
@@ -35,8 +27,6 @@ while(len(y) < 10000):
     edg = e[idx];
     G = nx.empty_graph(10);
     G.add_edges_from(edg)
-    
-    #print(mat)
 
     label = int(is_semieulerian(G) > 0)
     if(label == 0):
@@ -56,19 +46,13 @@ while(len(y) < 10000):
         X.append(mat.reshape(1,-1)[0]);
         y.append(label)
         print(i,label)
-
     count += label
-    #print(i,label)
-    #nx.draw(G);
-    #plt.show();
 
 y = np.asarray(y)
 X = np.asarray(X)
 perm = np.random.permutation(len(y))
-#print(perm)
 X = X[perm]
 y = y[perm]
-#print(y)
 import pickle
 pickle_out = open('eulerian.pickle','wb')
 pickle.dump((X,y),pickle_out);
@@ -80,4 +64,4 @@ print("X shape",len(X),X[0].shape)
 print("y shape",len(y))
 
 print("DONE")
-print("# Connected :",count)
+print("# Eulerian :",count)
