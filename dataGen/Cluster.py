9f4fa19280ed6d3faf0632a285c028a0c7d4fc0f
broodingmonk98 <es16btech11014@iith.ac.in>
2018-05-09 02:22:52 +0530

    Data sets generation

---
+++ dataGen/Cluster.py
@@ -0,0 +1,43 @@
+import numpy as np
+import networkx as nx
+import matplotlib.pyplot as plt
+
+G = nx.complete_graph(10)
+e = np.array(G.edges)
+print(G.edges,len(e))
+X,y = [],[]
+count = 0;
+for i in range(10000):
+    sample = np.random.normal(22,10);
+    if sample < 0 : sample = 2;
+    elif sample>45: sample = 42;
+    idx= np.random.permutation(len(e))[:int(sample)]
+    #print(idx)
+    edg = e[idx];
+    G = nx.empty_graph(10)
+    G.add_edges_from(edg)
+    mat = np.zeros((10,10),dtype=int)
+    for i in edg:
+        mat[tuple(i)]=True;
+        mat[tuple(i[::-1])]=True;
+    X.append(mat.reshape(1,-1)[0])
+
+    label = nx.average_clustering(G);
+    y.append(int(label))
+    count += label
+    print(i,label)
+
+y = np.asarray(y)
+X = np.asarray(X)
+import pickle
+pickle_out = open('hamil.pickle','wb')
+pickle.dump((X,y),pickle_out);
+pickle_out.close();
+pickle_in  = open('components.pickle','rb')
+X,y = pickle.load(pickle_in);
+print(type(X),type(X[0][0]))
+print("X shape",len(X),X[0].shape)
+print("y shape",len(y))
+
+print("DONE")
+print("# Ham cycles :",count)
